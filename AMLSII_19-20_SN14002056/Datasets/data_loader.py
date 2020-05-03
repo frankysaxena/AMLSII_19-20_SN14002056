a1f8399979361efced6b4ca00fb772eb8864efb7
Franky Saxena <fsaxena@cisco.com>
2020-05-03 23:44:19 +0100

    Data Loader file added

---
+++ AMLSII_19-20_SN14002056/Datasets/data_loader.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+class LoaderObject:
+
+    def __init__(self, path, txtfile):
+
+        self.path = path
+        self.txtfile = txtfile
+
+    def get_raw_dataframe(self, path, txtfile):
+        
+        """ Using a simple pandas read_csv import to extract labels from the dataset """
+        
+        return pd.read_csv(self.path + '/Datasets/' + self.txtfile  + '.csv', sep=',', header=None)
+
--- AMLSII_19-20_SN14002056/main.py
+++ AMLSII_19-20_SN14002056/main.py
@@ -1,33 +1,55 @@
-import ...
+from Datasets.data_loader import LoaderObject
+
+
+# # ======================================================================================================================
+# # Please change this path to the relevant path you're running this code from 
+
+path_to_dir = '/Users/fsaxena/Documents/UCL/amls2/AMLSII_19-20_SN14002056/AMLSII_19-20_SN14002056'
+
+# # ======================================================================================================================
+# # Constants
+
+taskA = 'taskA'
+taskB = 'taskB'
 
 # ======================================================================================================================
 # Data preprocessing
-data_train, data_val, data_test = data_preprocessing(args...)
-# ======================================================================================================================
-# Task A
-model_A = A(args...)                 # Build model object.
-acc_A_train = model_A.train(args...) # Train model based on the training set (you should fine-tune your model based on validation set.)
-acc_A_test = model_A.test(args...)   # Test model based on the test set.
+
+taskA = LoaderObject(path_to_dir, taskA)
+taskB = LoaderObject(path_to_dir, taskB)
+
+A_data = taskA.get_raw_dataframe(path_to_dir, taskA)
+B_data = taskB.get_raw_dataframe(path_to_dir, taskB)
+
+print(B_data)
 
 
-# Clean up memory/GPU etc...             # Some code to free memory if necessary.
+# data_train, data_val, data_test = data_preprocessing(args...)
+# # ======================================================================================================================
+# # Task A
+# model_A = A(args...)                 # Build model object.
+# acc_A_train = model_A.train(args...) # Train model based on the training set (you should fine-tune your model based on validation set.)
+# acc_A_test = model_A.test(args...)   # Test model based on the test set.
+
+
+# # Clean up memory/GPU etc...             # Some code to free memory if necessary.
 
 
 
-# ======================================================================================================================
-# Task B
-model_B = B(args...)
-acc_B_train = model_B.train(args...)
-acc_B_test = model_B.test(args...)
+# # ======================================================================================================================
+# # Task B
+# model_B = B(args...)
+# acc_B_train = model_B.train(args...)
+# acc_B_test = model_B.test(args...)
 
 
 
 
-# ======================================================================================================================
-## Print out your results with following format:
-print('TA:{},{};TB:{},{};'.format(acc_A_train, acc_A_test,
-                                                        acc_B_train, acc_B_test))
+# # ======================================================================================================================
+# ## Print out your results with following format:
+# print('TA:{},{};TB:{},{};'.format(acc_A_train, acc_A_test,
+#                                                         acc_B_train, acc_B_test))
 
-# If you are not able to finish a task, fill the corresponding variable with 'TBD'. For example:
-# acc_A_train = 'TBD'
-# acc_B_test = 'TBD'
+# # If you are not able to finish a task, fill the corresponding variable with 'TBD'. For example:
+# # acc_A_train = 'TBD'
+# # acc_B_test = 'TBD'
